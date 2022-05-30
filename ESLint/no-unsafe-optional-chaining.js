8a15bd2ee26f2fdb85a204d54948d7ddeb39dabc
cordis-dev <darius@adroiti.com>
2022-05-30 12:52:06 +0300

    Update no-unsafe-optional-chaining.js

--- ESLint/no-unsafe-optional-chaining.js
+++ ESLint/no-unsafe-optional-chaining.js
@@ -2,44 +2,4 @@
 
 (obj?.foo)();
 
-(obj?.foo).bar;
-
-(foo?.()).bar;
-
-(foo?.()).bar();
-
-(obj?.foo ?? obj?.bar)();
-
-(foo || obj?.foo)();
-
-(obj?.foo && foo)();
-
-(foo ? obj?.foo : bar)();
-
-(foo, obj?.bar).baz;
-
-(obj?.foo)`template`;
-
-new (obj?.foo)();
-
-[...obj?.foo];
-
-bar(...obj?.foo);
-
-1 in obj?.foo;
-
-bar instanceof obj?.foo;
-
-for (bar of obj?.foo);
-
-const { bar } = obj?.foo;
-
-[{ bar } = obj?.foo] = [];
-
-with (obj?.foo);
-
-class A extends obj?.foo {}
-
-var a = class A extends obj?.foo {};
-
 (obj?.foo)?.();
