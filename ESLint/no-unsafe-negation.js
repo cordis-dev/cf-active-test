89e7bb722edfdc72850c1ccb7da4d8eb602492b4
cordis-dev <darius@adroiti.com>
2022-05-30 12:47:46 +0300

    Create no-unsafe-negation.js

---
+++ ESLint/no-unsafe-negation.js
@@ -0,0 +1,10 @@
+/*eslint no-unsafe-negation: "error"*/
+
+if (!key in object) {
+    // operator precedence makes it equivalent to (!key) in object
+    // and type conversion makes it equivalent to (key ? "false" : "true") in object
+}
+
+if (!(key in object)) {
+    // key is not in object
+}
