520d68595b8401716aa38aa74fc564bc5de4e0e3
cordis-dev <darius@adroiti.com>
2022-05-30 10:44:43 +0300

    Update no-async-promise-executor.js

--- ESLint/no-async-promise-executor.js
+++ ESLint/no-async-promise-executor.js
@@ -11,3 +11,15 @@ const foo = new Promise(async (resolve, reject) => {
 const result = new Promise(async (resolve, reject) => {
   resolve(await foo);
 });
+
+const foo2 = new Promise((resolve, reject) => {
+  readFile('foo.txt', function(err, result) {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(result);
+    }
+  });
+});
+
+const result2 = Promise.resolve(foo);
