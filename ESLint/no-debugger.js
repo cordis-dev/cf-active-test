9fa9b36dc40043b727ebbfd738fac653830375fa
cordis-dev <darius@adroiti.com>
2022-05-30 11:00:18 +0300

    Create no-debugger.js

---
+++ ESLint/no-debugger.js
@@ -0,0 +1,10 @@
+/*eslint no-debugger: "error"*/
+
+function isTruthy(x) {
+    debugger;
+    return Boolean(x);
+}
+
+function isTruthy2(x) {
+    return Boolean(x); // set a breakpoint at this line
+}
