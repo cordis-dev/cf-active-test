f2eb02783c68a9e0a65fdebe1be870fc4c710b7b
cordis-dev <darius@adroiti.com>
2022-05-30 12:30:45 +0300

    Create no-setter-return.js

---
+++ ESLint/no-setter-return.js
@@ -0,0 +1,17 @@
+/*eslint no-setter-return: "error"*/
+
+var foo = {
+    set a(value) {
+        this.val = value;
+        return value;
+    }
+};
+
+var foo2 = {
+    set a(value) {
+        this.val = value;
+    }
+};
+
+console.log(foo);
+console.log(foo2);
