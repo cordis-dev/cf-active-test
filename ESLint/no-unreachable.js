b646fe19b33f7ae6cf21773868cb872c73feb723
cordis-dev <darius@adroiti.com>
2022-05-30 12:39:57 +0300

    Create no-unreachable.js

---
+++ ESLint/no-unreachable.js
@@ -0,0 +1,16 @@
+/*eslint no-unreachable: "error"*/
+
+function foo() {
+    return true;
+    console.log("done");
+}
+
+function foo2() {
+    return bar();
+    function bar() {
+        return 1;
+    }
+}
+
+foo();
+foo2();
