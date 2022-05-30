2eb86826460e185aadd5dd421ce2f22e52e12ef4
cordis-dev <darius@adroiti.com>
2022-05-30 11:01:53 +0300

    Create no-dupe-args.js

---
+++ ESLint/no-dupe-args.js
@@ -0,0 +1,10 @@
+/*eslint no-dupe-args: "error"*/
+
+function foo(a, b, a) {
+    console.log("value of the second a:", a);
+    console.log(b);
+}
+
+function foo2(a, b, c) {
+    console.log(a, b, c);
+}
