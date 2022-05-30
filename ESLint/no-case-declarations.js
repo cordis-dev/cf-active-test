03584f4acc5e557134b17ca462d975caa9df6718
cordis-dev <darius@adroiti.com>
2022-05-30 13:06:25 +0300

    Create no-case-declarations.js

---
+++ ESLint/no-case-declarations.js
@@ -0,0 +1,23 @@
+/*eslint no-case-declarations: "error"*/
+/*eslint-env es6*/
+
+switch (foo) {
+    case 1:
+        let x = 1;
+        console.log(x);
+        break;
+    default:
+        //
+}
+
+switch (foo) {
+    // The following case clauses are wrapped into blocks using brackets
+    case 1: {
+        let x = 1;
+        console.log(x);
+        break;
+    }
+    default: {
+        //
+    }
+}
