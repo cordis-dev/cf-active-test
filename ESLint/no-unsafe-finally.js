610effb02400e59cf5df0d10bf871d518bec3211
cordis-dev <darius@adroiti.com>
2022-05-30 12:46:41 +0300

    Update no-unsafe-finally.js

--- ESLint/no-unsafe-finally.js
+++ ESLint/no-unsafe-finally.js
@@ -2,8 +2,6 @@
 let foo = function() {
     try {
         return 1;
-    } catch(err) {
-        return 2;
     } finally {
         return 3;
     }
@@ -12,8 +10,6 @@ let foo = function() {
 let foo2 = function() {
     try {
         return 1;
-    } catch(err) {
-        return 2;
     } finally {
         console.log("hola!");
     }
