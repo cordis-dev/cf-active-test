89023e15c4619e12f8fd7073427a9d3babcb6c7d
cordis-dev <darius@adroiti.com>
2022-06-02 10:06:36 +0300

    Create adjacent-overload-signatures.js

---
+++ ESLint-TS/adjacent-overload-signatures.js
@@ -0,0 +1,13 @@
+declare namespace Foo {
+  export function foo(s: string): void;
+  export function foo(n: number): void;
+  export function bar(): void;
+  export function foo(sn: string | number): void;
+}
+
+declare namespace Foo2 {
+  export function foo(s: string): void;
+  export function foo(n: number): void;
+  export function foo(sn: string | number): void;
+  export function bar(): void;
+}
