f6a49a20d917df23d5aefc68b6e9ed36ffb2be69
cordis-dev <darius@adroiti.com>
2022-06-07 11:09:23 +0300

    Update ban-ts-comment.ts

--- ESLint-TS/ban-ts-comment.ts
+++ ESLint-TS/ban-ts-comment.ts
@@ -1,15 +1,4 @@
-interface Options {
-  'ts-expect-error'?: boolean | 'allow-with-description';
-  'ts-ignore'?: boolean | 'allow-with-description';
-  'ts-nocheck'?: boolean | 'allow-with-description';
-  'ts-check'?: boolean | 'allow-with-description';
-  minimumDescriptionLength?: number;
+if (x === false) {
+  // @ts-ignore: Unreachable code error
+  console.log('hello');
 }
-
-const defaultOptions: Options = {
-  'ts-expect-error': 'allow-with-description',
-  'ts-ignore': true,
-  'ts-nocheck': true,
-  'ts-check': false,
-  minimumDescriptionLength: 3,
-};
