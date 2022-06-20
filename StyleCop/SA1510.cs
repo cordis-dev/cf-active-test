ad8ddae1ed0bb7da70877d0fd68dcdf03ca2e294
cordis-dev <darius@adroiti.com>
2022-06-20 10:56:46 +0300

    Create SA1510.cs

---
+++ StyleCop/SA1510.cs
@@ -0,0 +1,15 @@
+public class SA1510
+{
+  public class Test()
+  {
+    try
+    {
+        SomeMethod();
+    }
+
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
+  }
+}
