2b618550a2c9bb06717b4d94ac6dc526508ce16d
cordis-dev <darius@adroiti.com>
2022-06-21 10:01:33 +0300

    Update EmptyBlock.java

--- checkstyle/EmptyBlock.java
+++ checkstyle/EmptyBlock.java
@@ -1,9 +1,12 @@
 public class EmptyBlock {
   private void t() {
     try { // violation
-
     } catch (Exception e) {
-      // ignored
+    }
+    
+    try {
+    }
+    finally {
     }
   }
 }
