a78eb9254f265a734f4ec53ca44c4ee9cb87f522
cordis-dev <darius@adroiti.com>
2022-06-21 10:14:43 +0300

    Create EqualsHashCode.java

---
+++ checkstyle/EqualsHashCode.java
@@ -0,0 +1,8 @@
+public static class EqualsHashCode {
+    public int hashCode() {
+        // code
+    }
+    public boolean equals(String o) { // violation, overloaded implementation of 'equals'
+        // code
+    }
+}
