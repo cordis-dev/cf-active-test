14049469137ca474b75d66309fd6f3bf88062d56
cordis-dev <darius@adroiti.com>
2022-06-21 09:53:09 +0300

    Update and rename IllegalTokenText.java to AvoidEscapedUnicodeCharacters.java

--- checkstyle/AvoidEscapedUnicodeCharacters.java
+++ checkstyle/AvoidEscapedUnicodeCharacters.java
@@ -1,4 +1,4 @@
-public class IllegalTokenText {
+public class AvoidEscapedUnicodeCharacters {
     public static void main(String[] args) {
         // \u0055 is a Unicode escape for the capital U character (U)
         System.out.println("Hello \u0055nicode".length());
