b06a83379c0d884ef42d5ac951fc03dc8529b580
cordis-dev <darius@adroiti.com>
2022-09-05 11:40:31 +0300

    Update NonExecutableCode.php

--- PHPCS/NonExecutableCode.php
+++ PHPCS/NonExecutableCode.php
@@ -3,19 +3,8 @@ switch ($var) {
     case '1':
         return;
         echo 'hi';
-
-    case '2':
-    case '3':
-        if ($something === true) {
-            break;
-            echo 'hi';
-        }
-    break;
     default:
-        return;
-
         if ($something === true) {
-            break;
             echo 'hi';
         }
 
