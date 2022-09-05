d28b2a97742888adbcc651a5e9758f92e7554721
cordis-dev <darius@adroiti.com>
2022-09-05 11:39:54 +0300

    Create NonExecutableCode.php

---
+++ PHPCS/NonExecutableCode.php
@@ -0,0 +1,22 @@
+<?php
+switch ($var) {
+    case '1':
+        return;
+        echo 'hi';
+
+    case '2':
+    case '3':
+        if ($something === true) {
+            break;
+            echo 'hi';
+        }
+    break;
+    default:
+        return;
+
+        if ($something === true) {
+            break;
+            echo 'hi';
+        }
+
+}
