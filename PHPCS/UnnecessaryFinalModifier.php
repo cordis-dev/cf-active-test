0f946e7cfbce13022758b2fc690908bfbfbe694a
cordis-dev <darius@adroiti.com>
2022-09-05 11:21:39 +0300

    Update UnnecessaryFinalModifier.php

--- PHPCS/UnnecessaryFinalModifier.php
+++ PHPCS/UnnecessaryFinalModifier.php
@@ -3,10 +3,4 @@
 final class Foo_Bar {
     public $foobar;
     public final function fooBar() {}
-
-    protected function foo() {}
-    protected final function fool() {}
-
-    private function Bar() {}
-    private final function Bard() {}
 }
