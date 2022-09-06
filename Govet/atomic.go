2def8f0495e0ea27628e18f8341d208d8a1ad00e
cordis-dev <darius@adroiti.com>
2022-09-06 10:57:40 +0300

    Update atomic.go

--- Govet/atomic.go
+++ Govet/atomic.go
@@ -1,4 +1,16 @@
-func AtomicTest() {
+package testdata
+
+import (
+	"sync/atomic"
+)
+
+type Counter uint64
+
+func AtomicTests() {
 	x := uint64(1)
-	x = atomic.AddUint64(&x, 1) // direct assignment to atomic value
+	x = atomic.AddUint64(&x, 1)        // ERROR "direct assignment to atomic value"
+	_, x = 10, atomic.AddUint64(&x, 1) // ERROR "direct assignment to atomic value"
+	x, _ = atomic.AddUint64(&x, 1), 10 // ERROR "direct assignment to atomic value"
+
+	y := &x
 }
