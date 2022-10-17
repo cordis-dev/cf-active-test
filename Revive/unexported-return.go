96debd79eccf2ec5150f166193f0149a222dd30e
cordis-dev <darius@adroiti.com>
2022-10-17 08:13:41 +0000

    Update unexported-return.go

--- Revive/unexported-return.go
+++ Revive/unexported-return.go
@@ -1,5 +1,17 @@
 package revive
 
+// TODO(matloob): Choose better names for Fun, Bloc, Goto, etc.
+// TODO(matloob): Write a parser for the Func disassembly. Maybe
+// the parser can be used instead of Fun.
+import (
+	"cmd/compile/internal/types"
+	"cmd/internal/obj"
+	"cmd/internal/src"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
 func Eq(cond, sub, alt string) ctrl {
 	return ctrl{BlockAMD64EQ, cond, []string{sub, alt}}
 }
