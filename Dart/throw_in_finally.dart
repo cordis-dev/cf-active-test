3723b11aa44758978d3cc9b4a03c0910080d528e
cordis-dev <darius@adroiti.com>
2024-06-06 16:23:12 +0300

    Create throw_in_finally.dart

---
+++ Dart/throw_in_finally.dart
@@ -0,0 +1,11 @@
+class BadThrow {
+  double nonCompliantMethod() {
+    try {
+      print('hello world! ${1 / 0}');
+    } catch (e) {
+      print(e);
+    } finally {
+      throw 'Find the hidden error :P'; // LINT
+    }
+  }
+}
