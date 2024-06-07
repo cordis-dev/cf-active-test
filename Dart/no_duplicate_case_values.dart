c343d489f57869124fd6430db8b43bf519fc9a14
cordis-dev <darius@adroiti.com>
2024-06-07 11:08:00 +0300

    Update no_duplicate_case_values.dart

--- Dart/no_duplicate_case_values.dart
+++ Dart/no_duplicate_case_values.dart
@@ -1,7 +1,13 @@
-const int A = 1;
-switch (v) {
-  case 1:
-  case 2:
-  case A:
-  case 2:
+void switchInt() {
+  const int A = 1;
+  int v = 5;
+
+  switch (v) {
+    case 1:
+    case 2:
+    case A:
+    case 2:
+    case 3:
+    default:
+  }
 }
