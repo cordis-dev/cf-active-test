db9c7670dee5cf6b1fa16200e69540eb2d8d82a4
cordis-dev <darius@adroiti.com>
2024-06-18 09:51:32 +0300

    Create t.cs

---
+++ t.cs
@@ -0,0 +1,15 @@
+public class SA1507_v5
+{
+  public bool Enabled
+  {
+      get 
+      { 
+          Console.WriteLine("Getting the enabled flag.");
+
+
+
+
+          return this.enabled; 
+      }
+  }
+}
