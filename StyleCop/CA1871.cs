ab5c8fe7bc3bfaf85121e5f17d839ec7ce54fbcd
Darius <darius@adroiti.com>
2025-01-28 15:35:19 +0200

    Add CA1871/CA1872, upgrade to .NET 9

---
+++ StyleCop/CA1871.cs
@@ -0,0 +1,10 @@
+﻿namespace App;
+
+internal class CA1871
+{
+    public static void Print(int? value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        Console.WriteLine(value.Value);
+    }
+}
---
+++ StyleCop/CA1872.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace App;
+
+public class CA1872
+{
+    private readonly byte[] _data = Encoding.ASCII.GetBytes("Hello World");
+
+    public string Encode()
+    {
+        return BitConverter.ToString(_data).Replace("-", "");
+    }
+
+    public string EncodeToLower()
+    {
+        return BitConverter.ToString(_data).Replace("-", "").ToLower();
+    }
+}
