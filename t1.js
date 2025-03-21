03a69c96831af2569d604043a3e7378f023aa745
codefactor-io <support@codefactor.io>
2025-03-21 10:16:19 +0000

    [CodeFactor] Apply fixes

--- t1.js
+++ t1.js
@@ -3,7 +3,7 @@ if (!!a)
   
 }
 
-if (!key in object) {
+if (!(key in object)) {
     // operator precedence makes it equivalent to (!key) in object
     // and type conversion makes it equivalent to (key ? "false" : "true") in object
 }
