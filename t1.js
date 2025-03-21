47e10c8269f9a28275baad34ef6372a18ecb7ee4
cordis-dev <darius@adroiti.com>
2025-03-21 12:15:55 +0200

    Update t1.js

--- t1.js
+++ t1.js
@@ -2,3 +2,8 @@ if (!!a)
 {
   
 }
+
+if (!key in object) {
+    // operator precedence makes it equivalent to (!key) in object
+    // and type conversion makes it equivalent to (key ? "false" : "true") in object
+}
