3d68ff316621c9c45b81b1a509fcb76e83bd7d0a
cordis-dev <darius@adroiti.com>
2025-12-15 16:24:07 +0200

    Update B103.py

--- Bandit/B103.py
+++ Bandit/B103.py
@@ -1,7 +1,5 @@
 import os
 import stat
 
-os.chmod('~/.bashrc', 511)
-os.chmod('/etc/hosts', 0o777)
+os.chmod('/etc/passwd', 0o760)
 os.chmod('/tmp/oh_hai', 0x1ff)
-os.chmod('/etc/passwd', stat.S_IRWXU)
