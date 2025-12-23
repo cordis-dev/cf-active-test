d2303300f209fa0d7036b855d4514dfe8091b1c4
cordis-dev <darius@adroiti.com>
2025-12-23 10:26:34 +0200

    Create CA2023.cs

---
+++ StyleCop/CA2023.cs
@@ -0,0 +1,20 @@
+﻿namespace App;
+
+internal class CA2023
+{
+    private readonly ILogger _logger;
+
+    public CA2023(ILogger<Example> logger)
+    {
+        _logger = logger;
+    }
+
+    public void LogData(string name, int value)
+    {
+        // Violation: unmatched opening brace.
+        _logger.LogInformation("Processing {Name with value {Value}", name, value);
+
+        // Violation: unmatched closing brace.
+        _logger.LogInformation("Processing Name} with value {Value}", name, value);
+    }
+}
